13064e988beba4db2022e8574185bfab8adca12a
Mahak Gupta <161015591+mahakitis@users.noreply.github.com>
2025-07-23 19:43:10 +0530

    Delete contextbasedbot directory

--- contextbasedbot/chat/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Document
-
-class DocumentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Document
-        fields = ['doc_id', 'title', 'doc_type', 'added_at']
--- contextbasedbot/chat/services/web_scraper.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-def scrap_website(url):
-    response = requests.get(url, timeout=10)
-    soup = BeautifulSoup(response.text, "html.parser")
-    text = soup.get_text(separator=" ", strip=True)
-    return text
